afb3d6c47bd39fb1125148e20c332fc81b4fa447
clbokea <clbo@kea.dk>
2019-08-29 16:48:59 +0200

    hello added

---
+++ code_from_today/hello.py
@@ -0,0 +1,17 @@
+import sys
+
+def hello(name):
+    if name == 'Alice':
+      name = name + '?????'
+    else:
+      does_not_exist(names)
+      SFDKJSLKRYIWRYWHjscjhg
+
+    print(name)
+
+def main():
+    hello(sys.argv[1])
+
+main()
+
+
